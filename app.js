11d47f1f698669a083cb6a0d453be96f3e75ebe8
Nithindas004 <nithindas232001@gmail.com>
2024-02-13 06:29:40 +0530

    add security completed

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const adminRoute = require("./controller/adminRouter")
+
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://nithindas1234:1234nith@cluster0.lvn9hia.mongodb.net/visitorDb?retryWrites=true&w=majority",
+{useNewUrlParser: true})
+
+app.use("/api/admin",adminRoute)
+
+app.listen(3001,()=>{
+    console.log("server running....")
+})
